9d4ba126e5463439bba192c37bce583d26cdca7f
Dr Alvin Ang <60359452+DRALVINANG@users.noreply.github.com>
2024-11-20 13:32:01 +0800

    Update Log_Reg_on_Breast_Cancer.py

--- Machine Learning/Log_Reg_on_Breast_Cancer.py
+++ Machine Learning/Log_Reg_on_Breast_Cancer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
