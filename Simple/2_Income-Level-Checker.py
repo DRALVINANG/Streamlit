1429eb9a0b80add04debf7a944352710968788c9
Dr Alvin Ang <60359452+DRALVINANG@users.noreply.github.com>
2025-11-23 12:28:14 +0800

    Update 2_Income-Level-Checker.py

--- Simple/2_Income-Level-Checker.py
+++ Simple/2_Income-Level-Checker.py
@@ -4,13 +4,13 @@ st.title("Income Level Checker")
 
 income = st.number_input("Enter your income", step=1)
 
-if income > 8000:
+if income >= 20000:
     result = "High income"
-elif income > 4000:
+elif income >= 15000:
     result = "Medium high income"
-elif income > 2000:
+elif income >= 10000:
     result = "Medium income"
-elif income > 1000:
+elif income >= 8000:
     result = "Medium low income"
 else:
     result = "Low income"
