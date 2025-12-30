dfe1716590e996627a330e764b3f51e9d36f6f38
Dr Alvin Ang <60359452+DRALVINANG@users.noreply.github.com>
2025-12-30 18:59:45 +0800

    Update 2_Income-Level-Checker.py

--- Simple/2_Income-Level-Checker.py
+++ Simple/2_Income-Level-Checker.py
@@ -5,14 +5,15 @@ st.title("Income Level Checker")
 income = st.number_input("Enter your income", step=1)
 
 if income >= 20000:
-    result = """<span style="color:red;">$$$ you are SUPER HIGH INCOME.............$$ no wonder drive Ferrari$........can please PAYNOW Alvin $8888? ........Alvin is really very very poor.....</span>"""
+    result = """<span style="color:green;">$$$ you are SUPER HIGH INCOME.............$$ no wonder drive Ferrari$........can please PAYNOW Alvin $8888? ........Alvin is really very very poor.....</span>"""
 elif income >= 15000:
     result = """<span style="color:green;">$$$ you are High income.........$$ no wonder wear Rolex$............please donate your CDC vouchers to Alvin please?...... Alvin needs to buy rice....</span>"""
 elif income >= 10000:
     result = """<span style="color:orange;">$$$ you are Median High income....... $$ no wonder stay Orchard$....... can treat Alvin eat Chicken Chop Please?........YOU ARE SO RICH STILL SO STINGY???</span>"""
 elif income >= 8000:
-    result = """<span style="color:blue;">$$$ you are still High income........$$ can buy Alvin Starbucks or Luckin Coffee$ PLEASE? .................YOU STILL SO STINGY???</span>"""
+    result = """<span style="color:orange;">$$$ you are still High income........$$ can buy Alvin Starbucks or Luckin Coffee$ PLEASE? .................YOU STILL SO STINGY???</span>"""
 else:
     result = """<span style="color:red;">Alvin POK KAI NO MONEY!!!..........</span>"""
 
-st.markdown(f"$$${income}$$ = {result}", unsafe_allow_html=True)
+# Use Markdown to render the result with HTML
+st.markdown(f"${income} = {result}", unsafe_allow_html=True)
