997c8b74368f652ca5ed4691623d593b38a4bf68
Dr Alvin Ang <60359452+DRALVINANG@users.noreply.github.com>
2025-12-30 19:05:37 +0800

    Update 2_Income-Level-Checker.py

--- Simple/2_Income-Level-Checker.py
+++ Simple/2_Income-Level-Checker.py
@@ -5,13 +5,13 @@ st.title("Income Level Checker")
 income = st.number_input("Enter your income", step=1)
 
 if income >= 20000:
-    result = f"""<span style="color:red;">${income} = $$$ you are SUPER HIGH INCOME.............$$ no wonder drive Ferrari$........can please PAYNOW Alvin $8888? ........Alvin is really very very poor.....</span>"""
+    result = f"""<span style="color:red;">$${income}$$ = $$$ you are SUPER HIGH INCOME.............$$ no wonder drive Ferrari $$........can please PAYNOW Alvin $8888? ........Alvin is really very very poor.....</span>"""
 elif income >= 15000:
-    result = f"""<span style="color:green;">${income} = $$$ you are High income.........$$ no wonder wear Rolex$............please donate your CDC vouchers to Alvin please?...... Alvin needs to buy rice....</span>"""
+    result = f"""<span style="color:green;">$${income}$$ = $$$ you are High income.........$$ no wonder wear Rolex $$............please donate your CDC vouchers to Alvin please?...... Alvin needs to buy rice....</span>"""
 elif income >= 10000:
-    result = f"""<span style="color:orange;">${income} = $$$ you are Median High income....... $$ no wonder stay Orchard$....... can treat Alvin eat Chicken Chop Please?........YOU ARE SO RICH STILL SO STINGY???</span>"""
+    result = f"""<span style="color:orange;">$${income}$$ = $$$ you are Median High income....... $$ no wonder stay Orchard $$....... can treat Alvin eat Chicken Chop Please?........YOU ARE SO RICH STILL SO STINGY???</span>"""
 elif income >= 8000:
-    result = f"""<span style="color:yellow;">${income} = $$$ you are still High income........$$ can buy Alvin Starbucks or Luckin Coffee$ PLEASE? .................YOU STILL SO STINGY???</span>"""
+    result = f"""<span style="color:yellow;">$${income}$$ = $$$ you are still High income........$$ can buy Alvin Starbucks or Luckin Coffee $$ PLEASE? .................YOU STILL SO STINGY???</span>"""
 else:
     result = f"""<span style="color:red;">${income} = Alvin POK KAI NO MONEY!!!..........</span>"""
 
