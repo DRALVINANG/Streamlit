c715be017b333429c6e0ed2a837e8709b9916566
Dr Alvin Ang <60359452+DRALVINANG@users.noreply.github.com>
2025-12-30 18:15:08 +0800

    Update 2_Income-Level-Checker.py

--- Simple/2_Income-Level-Checker.py
+++ Simple/2_Income-Level-Checker.py
@@ -5,14 +5,14 @@ st.title("Income Level Checker")
 income = st.number_input("Enter your income", step=1)
 
 if income >= 20000:
-    result = "SUPER HIGH INCOME -> no wonder drive Ferrari -> KINDLY PAYNOW Alvin $88888 THANK YOU SO MUCH! Alvin is really very very poor..SO RICH STILL SO STINGY???"
+    result = "you are SUPER HIGH INCOME.............no wonder drive Ferrari........can please PAYNOW Alvin $88888?........ Alvin is really very very poor....."
 elif income >= 15000:
-    result = "High income -> no wonder wear Rolex -> PLEASE DONATE ALVIN YOUR CDC VOUCHERS THANK YOU... Alvin needs to buy rice....SO RICH STILL SO STINGY???"
+    result = "you are High income.........no wonder wear Rolex............please donate your CDC vouchers to Alvin please?...... Alvin needs to buy rice...."
 elif income >= 10000:
-    result = "Median High income -> no wonder stay Orchard -> can treat Alvin eat Chicken Chop Please? SO RICH STILL SO STINGY???"
+    result = "you are Median High income....... no wonder stay Orchard....... can treat Alvin eat Chicken Chop Please?........YOU ARE SO RICH STILL SO STINGY???"
 elif income >= 8000:
-    result = "Also High income -> can buy Alvin Starbucks or Luckin Coffee PLEASE? SO RICH STILL SO STINGY???"
+    result = "you are still High income........can buy Alvin Starbucks or Luckin Coffee PLEASE? .................YOU STILL SO STINGY???"
 else:
-    result = "Alvin POK KAI NO MONEY!!!"
+    result = "Alvin POK KAI NO MONEY!!!.........."
 
 st.write(f"{income} = {result}")
